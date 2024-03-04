cd6243179bceb2fdf1a095a3064b6a9c8c7febac
Thorsten Heimes <thorsten.heimes@bergzeit.de>
2024-03-04 13:27:43 +0100

    Test exponential backoff

--- backoff_test.go
+++ backoff_test.go
@@ -1,1 +1,31 @@
 package lazyhttp_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/niksteff/lazyhttp"
+)
+
+func TestBackoff(t *testing.T) {
+	retries := 5
+	b := lazyhttp.NewExponentialBackoff(5*time.Second, 2*time.Hour, retries)
+
+	// Test if the backoff time increases exponentially
+	prev, _ := b.Backoff()
+	t.Logf("initial offset = %s", prev)
+	for i := 0; i < retries-1; i++ {
+		next, _ := b.Backoff()
+		t.Logf("prev = %v, next = %v", prev, next)
+		if next <= prev {
+			t.Errorf("Backoff time did not increase: prev = %v, next = %v", prev, next)
+		}
+		prev = next
+	}
+
+	// Test if the retry limit is respected
+	_, ok := b.Backoff()
+	if ok {
+		t.Errorf("Retry limit not respected: expected = false, got = %v", ok)
+	}
+}
